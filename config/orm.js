5af87327d2998ab0849856e69711c81609fae83a
Wilbur125 <matt.s.wilbur@gmail.com>
2020-12-13 14:32:07 -0800

    create and establish orm.js and methods selectAll, insertOne, and updateOne

---
+++ config/orm.js
@@ -0,0 +1,15 @@
+const {connection} = require("./connection.js");
+
+function selectAll() {
+
+}
+
+function insertOne() {
+
+}
+
+function updateOne() {
+    
+}
+
+module.exports = {selectAll, insertOne, updateOne};
