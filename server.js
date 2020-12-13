ba6bae8beddcbef9280813be2cf0072f0d6022cd
Wilbur125 <matt.s.wilbur@gmail.com>
2020-12-13 15:30:22 -0800

    file updated to connect to server and include all required packages

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+var exphbs = require("express-handlebars");
+var routes = require("./controllers/burgers_controller.js")
+
+var PORT = process.env.PORT || 3306;
+var app = express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+app.use(routes);
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+  });
